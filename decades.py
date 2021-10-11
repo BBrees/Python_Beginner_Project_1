12ea5f18f55db08c9dbb7eeaccdf52bd50153d2b
Beckett Brees <bbreespt@gmail.com>
2021-10-10 20:56:26 -0400

    add decade calculator

---
+++ decades.py
@@ -0,0 +1,11 @@
+
+# User type age; string is converted to integer; input on new line
+age = int(input("How old are you?\n"))
+
+# Uses age variable divided by 10, decade calculate and converted from float to integer
+decade = int(age/10)
+
+# Function displays prior inputs, variable decade is converted from integer to string
+print("You are " + str(decade) + " decades")
+
+
