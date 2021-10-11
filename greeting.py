0ec2e0e79a2c25c96a3c9f02f70cf38017d2be63
Beckett Brees <bbreespt@gmail.com>
2021-10-10 20:40:40 -0400

    Greeting Program Added

---
+++ greeting.py
@@ -0,0 +1,9 @@
+# User enters name
+hello = "Hello"
+name = input("What is your name?")
+
+# User information incorporated into string
+greeting = hello + " " + name
+
+# Greeting printed
+print(greeting)
