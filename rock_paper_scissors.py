8ec1d750839a893429ea67bd7ae4b4b3f6f0a385
Beckett Brees <bbreespt@gmail.com>
2021-10-16 07:48:20 -0400

    imported random

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,16 @@
-computer_choice = 'scissors'
+import random
+
+computer_choice = random.choice(['rock', 'scissors', 'paper'])
 
 user_choice = input('Do you want - rock, paper, or scissors?\n')
 
 if computer_choice == str.lower(user_choice):
     print('Try again - no one wins - TIED')
+elif str.lower(user_choice) == 'rock' and computer_choice == 'scissors':
+    print('You smashed the scissors and win!')
+elif str.lower(user_choice) == 'paper' and computer_choice == 'rock':
+    print('You covered the rock and win!')
+elif str.lower(user_choice) == 'scissors' and computer_choice == 'paper':
+    print('You cut it up and win!')
+else:
+    print('You lose, keep trying')
